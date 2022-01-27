b4ea3793f3d839d72bb190a97baf40ee879806a1
Radostin Petrov <radostin.petrov99@gmail.com>
2022-01-27 08:49:20 +0000

    Change Index page

--- src/main/java/com/develogical/web/IndexPage.java
+++ src/main/java/com/develogical/web/IndexPage.java
@@ -8,7 +8,7 @@ public class IndexPage extends HtmlPage {
     protected void writeContentTo(PrintWriter writer) {
         writer.println(
                 "<h1>Welcome!!</h1>" +
-                "<p>Enter your query in the box below (or don't, I don't care XP): " +
+                "<p>Enter your query in the box below (or don't, I don't care): " +
                   "<form><input type=\"text\" name=\"q\" />" +
                     "<input type=\"submit\">" +
                   "</form>" +
